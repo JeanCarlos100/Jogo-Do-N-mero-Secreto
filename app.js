5f12dc4581de74a103a6fe8e98a217c9a21e6bf1
Jean Carlos <santosjeancarlos70@gmail.com>
2024-02-03 13:41:19 -0300

    first commit

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-let numeroMaximo = 10;
+let numeroMaximo = 50;
 let listaDeNumerosSorteador = [];
 let numeroAleatorio = geraNumeroAleatorio();
 let tentativa = 1;
 
 function exibirTexteNTela(tag, texto) {
+
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2});
+
 }
+
 function exibirMessagemInicial() {
+
     exibirTexteNTela('h1','jogo do número secreto');
     exibirTexteNTela('p',`Você precisa adivinhar um número entre 1 e ${numeroMaximo}`);
+
 }
 
 exibirMessagemInicial();
 
 function verificarChute() {
+
     let chute = document.querySelector('input').value;
 
     if (chute == numeroAleatorio){
+
         exibirTexteNTela('h1', 'Acertou!');
+
         let palavraTentativar = tentativa > 1 ? 'tentativas' : 'tentativa';
         let menssagemTentativar = `'Você descobriu o número secreto com ${tentativa} ${palavraTentativar}!`;
+
         exibirTexteNTela('p', menssagemTentativar);
+
         document.getElementById('reiniciar').removeAttribute('disabled');
+
     } else{
+
         if (chute > numeroAleatorio) {
             exibirTexteNTela('p', 'O número secreto é menor');
         } else{
@@ -36,6 +48,7 @@ function verificarChute() {
 }
 
 function geraNumeroAleatorio () {
+
     let numeroEscolidor = parseInt(Math.random() * numeroMaximo + 1);
     let quantidadeDeElemeontoNaLista = listaDeNumerosSorteador.length;
 
@@ -53,14 +66,17 @@ function geraNumeroAleatorio () {
 }
 
 function limpaTela() {
+
     chute = document.querySelector('input');
     chute.value = '';
 }
 
 function reiniciarJogo() {
+
     numeroAleatorio = geraNumeroAleatorio();
     limpaTela();
     tentativa = 1;
     exibirMessagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled', true);
+    
 }
